cb5ffb9fa8f7035d4cfa52f2bdc97e972dd2c719
mita <cpjvmm@outlook.jp>
2021-11-28 17:09:13 +0900

    テンプレート操作処理をクラス分割

--- research_cpp/process/CarsTracer.h
+++ research_cpp/process/CarsTracer.h
@@ -5,6 +5,9 @@
 class ImgProc::CarsTracer
 {
 private:
+	class TemplateHandle;
+	friend class TemplateHandle;
+
 	std::vector<std::pair<size_t, uint64_t>> mDeleteLists;
 	Image mTempFrame;
 	Image mTemp;
@@ -22,7 +25,7 @@ private:
 	std::vector<std::unordered_set<uint64_t>> mMatchLabels;
 public:
 	CarsTracer() 
-	{ 
+	{
 		mLabelNum = 0;
 		mMatchLabels.resize(ImgProcToolkit::sRoadMasksNum);
 	}
@@ -38,36 +41,4 @@ private:
 	void DetectNewCars(const size_t& idx);
 
 	template<class T> void TraceByLabels(const size_t& idx, cv::Rect_<T>& carPos);
-
-	/// <summary>
-	/// テンプレートマッチングの対象領域を制限する
-	/// </summary>
-	/// <param name="maskId">道路マスク番号</param>
-	/// <param name="carId">車両番号</param>
-	/// <param name="orgMagni">拡大・縮小倍率</param>
-	/// <param name="orgMergin">車両の1フレーム後における推定移動幅</param>
-	void ExtractCarsNearestArea(const size_t& maskId, const uint64_t& carId, const double& orgMagni = 1.001, const int& orgMergin = 8);
-
-	/// <summary>
-	/// 横方向の負エッジをy方向微分によって求め, 切りだすy座標を処理によって選択
-	/// </summary>
-	/// <param name="inputImg">入力テンプレート画像</param>
-	/// <returns>切りだすy座標</returns>
-	int ExtractAreaByEdgeH(const Image& inputImg);
-
-	/// <summary>
-	/// 縦方向の正負両エッジをそれぞれx方向微分によって求め, 切りだすx座標二つを処理によって選択
-	/// 正エッジの左端, 負エッジの右端が出力されるはず
-	/// 逆の場合, 負エッジの左端, 正エッジの右端が出力される
-	/// </summary>
-	/// <param name="inputImg">入力テンプレート画像</param>
-	/// <returns>切りだすx座標二つを一組にして返す</returns>
-	std::pair<int, int> ExtractAreaByEdgeV(const Image& inputImg);
-
-	/// <summary>
-	/// 頻度値データを, 一行n列の1チャンネル(グレースケール)画像として考え, 極大値をもつインデックスを保存
-	/// </summary>
-	/// <param name="inputData">入力データ, 必ずcolsをn, rowを1にしておく</param>
-	/// <param name="retData">取得したいデータを格納するコンテナの参照</param>
-	void SplitLineByEdge(const Image& inputData, std::vector<int>& retData);
 };
---
+++ research_cpp/process/TemplateHandle.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "CarsTracer.h"
+
+class ImgProc::CarsTracer::TemplateHandle
+{
+public:
+	/// <summary>
+	/// テンプレートマッチングの対象領域を制限する
+	/// </summary>
+	/// <param name="nearRect">制限区域矩形</param>
+	/// <param name="maskId">道路マスク番号</param>
+	/// <param name="carId">車両番号</param>
+	/// <param name="orgMagni">拡大・縮小倍率</param>
+	/// <param name="orgMergin">車両の1フレーム後における推定移動幅</param>
+	static void ExtractCarsNearestArea(cv::Rect2d& nearRect, const size_t& maskId, const uint64_t& carId, const double& orgMagni = 1.001, const int& orgMergin = 8);
+
+	/// <summary>
+	/// 横方向の負エッジをy方向微分によって求め, 切りだすy座標を処理によって選択
+	/// </summary>
+	/// <param name="inputImg">入力テンプレート画像</param>
+	/// <returns>切りだすy座標</returns>
+	static int ExtractAreaByEdgeH(const Image& inputImg);
+
+	/// <summary>
+	/// 縦方向の正負両エッジをそれぞれx方向微分によって求め, 切りだすx座標二つを処理によって選択
+	/// 正エッジの左端, 負エッジの右端が出力されるはず
+	/// 逆の場合, 負エッジの左端, 正エッジの右端が出力される
+	/// </summary>
+	/// <param name="inputImg">入力テンプレート画像</param>
+	/// <returns>切りだすx座標二つを一組にして返す</returns>
+	static std::pair<int, int> ExtractAreaByEdgeV(const Image& inputImg);
+
+	/// <summary>
+	/// 頻度値データを, 一行n列の1チャンネル(グレースケール)画像として考え, 極大値をもつインデックスを保存
+	/// </summary>
+	/// <param name="inputData">入力データ, 必ずcolsをn, rowを1にしておく</param>
+	/// <param name="retData">取得したいデータを格納するコンテナの参照</param>
+	static void SplitLineByEdge(const Image& inputData, std::vector<int>& retData);
+};
+
